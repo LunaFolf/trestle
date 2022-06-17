7cf1855290fe5475923895a4abd79cb263d2fb7a
WhiskeeDev <email@liamholmes.co.uk>
2022-06-17 02:52:06 +0100

    Introduced non-backwards compatible changes, such as: * Majority of TrestleAPI configuration is now in the constructor. * Introduced automatic spec generation available via `getSpec()` on the TrestleAPI class. This is OpenAPI 3.0.0 compliant. * TrestleRoute now has additional values for describing routes/paths, necessary for spec. * `specStrict` is now available, and will prevent running if the API is not OpenAPI 3.0.0 spec compliant.

--- classes/TrestleAPI/index.js
+++ classes/TrestleAPI/index.js
@@ -1,6 +1,7 @@
 const { getPositiveFlavour } = require('./../../utils/flavourText')
 const url = require('url')
 const { TrestleRoute } = require('../TrestleRoute')
+const YAML = require('yaml')
 
 const titleCard = '[TrestleAPI]'.yellow
 
@@ -60,24 +61,39 @@ function handleJsonResponse (response, json, options) {
   response.end()
 }
 
-let beforeEachRouteFncs = []
+const beforeEachRouteFncs = []
 
 class TrestleAPI {
-  routes = []
-  options = {}
-  port = 443
-  debug = false
-  secureMode = true
+  // Storage based variables
+  _routes = []
+  _options = {}
+  _blockedIps = []
+  _validHosts = []
 
-  blockedIps = []
-  validHosts = []
+  // Important settings, like super duper important... I promise
+  _port = 443 // default port, requires secureMode to be true typically.
+  _secureMode = true // should SSL be used?
+  _debug = false // Should Trestle print debug info? (not properly implemented yet)
+  _specStrict = false // API should be OpenAPI 3.0.0 compliant? If true, the API will throw an error if non-compliant and refuse to start.
 
-  constructor({ port, options, debug, blockedIps, validHosts }) {
-    if (port) this.port = port
-    if (options) this.options = options
-    if (blockedIps) this.blockedIps = blockedIps
-    if (validHosts) this.validHosts = validHosts
-    if (debug !== undefined) this.debug = !!debug
+  // Other configuration based settings
+  _appName = 'TrestleAPI'
+  _appDescription = undefined
+  _appVersion = '1.0.0' // Not to be confused with the version number of the Framework
+
+  constructor({ port, options, debug, blockedIps, validHosts, secureMode, specStrict, appName, appDescription, appVersion }) {
+    if (port) this._port = port
+    if (options) this._options = options
+    if (blockedIps) this._blockedIps = blockedIps
+    if (validHosts) this._validHosts = validHosts
+
+    if (appName) this._appName = appName
+    if (appDescription) this._appDescription = appDescription
+    if (appVersion) this._appVersion = appVersion
+
+    if (debug !== undefined) this._debug = !!debug
+    if (secureMode !== undefined) this._secureMode = !!secureMode
+    if (specStrict !== undefined) this._specStrict = !!specStrict
   }
 
   addRoute(route) {
@@ -88,7 +104,7 @@ class TrestleAPI {
 
     console.log(titleCard, 'Added new route', `${route.public ? 'Public' : 'Private'}`.yellow ,`[${route.method}] ${route.path}`.cyan)
 
-    this.routes.push(route)
+    this._routes.push(route)
   }
 
   beforeEachRoute(callback) {
@@ -103,11 +119,55 @@ class TrestleAPI {
   setSSL(key, cert) {
     if (!key || !cert) throw new Error('Both SSL Key and Cert are required when defining SSL')
     console.log(titleCard, 'SSL Cert and Key OK, saving options now.')
-    this.options = { key, cert }
+    this._options = { key, cert }
+  }
+
+  // Return a YAML spec for the API, compatible with OpenAPI 3.0.0
+  getSpec(format = 'yaml') {
+    if (this._specStrict) {
+      const invalidRoute = this._routes.find(route => !route.isSpecCompatible())
+      if (invalidRoute) {
+        throw new Error(`API is not OpenAPI 3.0.0 compliant. Fix the following route or set specStrict to false: [${invalidRoute.method}] ${invalidRoute.path}`)
+      }
+    }
+
+    const spec = {
+      openapi: '3.0.0',
+      info: {
+        title: this._appName,
+        version: this._appVersion
+      },
+      servers: [{
+        url: `http${this._secureMode ? 's' : ''}://${url.parse(`http://${this._options.hostname || 'localhost'}:${this._port}`).host}`
+      }],
+      paths: {}
+    }
+
+    if (this._appDescription) spec.info.description = this._appDescription
+
+    this._routes.forEach(route => {
+      const { path, method, summary, description, responses, tags } = route
+
+      spec.paths[path] = {
+        [String(method).toLowerCase()]: {
+          summary,
+          description,
+          responses,
+          tags : tags.length ? tags : undefined
+        }
+      }
+    })
+
+    if (format === 'json') return spec
+
+    const openapiSpec = new YAML.Document()
+    openapiSpec.contents = spec
+
+    return openapiSpec.toString()
   }
 
   matchRoute (pathName, requestMethod) {
-    const routesWithMatchingMethod = this.routes.filter(route => {
+    const routesWithMatchingMethod = this._routes.filter(route => {
       const routeMethod = route.method || 'GET'
       return routeMethod === requestMethod
     })
@@ -157,7 +217,7 @@ class TrestleAPI {
       })
     }
 
-    if (this.debug) {
+    if (this._debug) {
       console.debug({
         routesWithMatchingMethod,
         paramLocations,
@@ -169,18 +229,18 @@ class TrestleAPI {
   }
 
   init() {
-    const self = this
+    const self = this // TODO: Check if this is still necessary
 
     let httpProtocol
 
-    if (self.secureMode) httpProtocol = require('https')
+    if (this._secureMode) httpProtocol = require('https')
     else {
       httpProtocol = require('http')
       console.warn(titleCard, 'WARNING: Running in insecure mode, this is not recommended.'.bgYellow.red)
     }
 
-    console.log(titleCard, `Port: ${this.port}`.cyan , 'Creating Listening Server...')
-    return httpProtocol.createServer(this.options, async function (request, response) {
+    console.log(titleCard, `Port: ${this._port}`.cyan , 'Creating Listening Server...')
+    return httpProtocol.createServer(this._options, async function (request, response) {
       const responseFncs = getResHelpers(response)
 
       const sourceIp = request.headers['x-forwarded-for'] || request.connection.remoteAddress
@@ -188,9 +248,9 @@ class TrestleAPI {
       const method = request.method
 
       // Check if the host is allowed
-      if (self.validHosts.length > 0) {
+      if (self._validHosts.length > 0) {
         const host = request.headers.host || 'NO HOST'
-        if (!self.validHosts.includes(host)) {
+        if (!self._validHosts.includes(host)) {
           console.error(titleCard, `Blocked request from ${sourceIp} for invalid host ${host}`.bgRed)
           responseFncs.error(403, 'Invalid Host')
           return
@@ -198,8 +258,8 @@ class TrestleAPI {
       }
 
       // Check if the IP is allowed
-      if (self.blockedIps.length > 0) {
-        if (self.blockedIps.includes(sourceIp)) {
+      if (self._blockedIps.length > 0) {
+        if (self._blockedIps.includes(sourceIp)) {
           console.error(titleCard, `Blocked request from ${sourceIp} for blocked IP`.bgRed)
           responseFncs.error(403, 'Blocked IP')
           return
@@ -265,7 +325,7 @@ class TrestleAPI {
 
           console.log(titleCard, sourceIp, `[${method}] ${q.path}`)
 
-          if (this.debug) {
+          if (this._debug) {
             console.debug({
               responseFncs,
               passBody,
@@ -287,14 +347,14 @@ class TrestleAPI {
 
           return true
         })
-    }).listen(this.port)
+    }).listen(this._port)
   }
 }
 
 function handlePreflight(response, { path, sourceIp, method }) {
   handleJsonResponse(response, null, { statusCode: 200 })
 
-  if (TrestleAPI.debug) console.debug(titleCard, sourceIp, `[${method}] ${path}`.yellow)
+  if (TrestleAPI._debug) console.debug(titleCard, sourceIp, `[${method}] ${path}`.yellow)
   return
 }
 
--- classes/TrestleRoute/index.js
+++ classes/TrestleRoute/index.js
@@ -5,7 +5,13 @@ class TrestleRoute extends EventEmitter {
   method = 'GET'
   public = false
 
-  constructor(path, options) {
+  // Required variables for creating openAPI Path spec
+  summary = undefined
+  description = undefined
+  responses = { 200: { description: 'Success' } }
+  tags = []
+
+  constructor(path, options, pathSpec) {
     super()
     if (!path) throw new Error("TrestleRoute 'path' field is a required parameter.")
     this.path = path
@@ -13,6 +19,45 @@ class TrestleRoute extends EventEmitter {
       this.method = options.method || 'GET'
       this.public = options.public == undefined ? false : options.public
     }
+
+    if (pathSpec) {
+      const { summary, description, responses, tags } = pathSpec
+
+      if (summary) this.setSummary(summary)
+      if (description) this.setDescription(description)
+      if (responses) this.setResponses(responses)
+      if (tags) this.setTags(tags)
+    }
+  }
+
+  setSummary (summary) {
+    this.summary = summary
+  }
+
+  setDescription (description) {
+    this.description = description
+  }
+
+  setResponses (responses) {
+    this.responses = responses
+  }
+
+  setTags (tags) {
+    this.tags = tags
+  }
+
+  isSpecCompatible () {
+    const summaryIsDefined = this.summary != undefined
+    const responsesAreDefined = Object.keys(this.responses).length > 0
+
+    const isValid = summaryIsDefined && responsesAreDefined
+
+    if (!isValid) console.error(this.method, this.path, 'is not spec compatible: ', {
+      summaryIsDefined,
+      responsesAreDefined
+    })
+
+    return isValid
   }
 
   handle (data) {
